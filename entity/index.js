d80f725d8f6767fd4908c27cb46c38ea4f69dff8
monjurmorshed793 <monjurmorshed793@gmail.com>
2022-06-02 14:30:11 +0600

    field add loop added

--- entity/index.js
+++ entity/index.js
@@ -1,17 +1,72 @@
 var Generator = require('yeoman-generator')
-
+var fields = {};
+var entity = {};
+var addField = {};
 module.exports = class extends Generator{
+
     constructor(args, opts) {
         super(args, opts);
+        this.fields = [];
 
         this.option('entity');
     }
 
-    method1(){
-        this.log('method 1 just ran');
+    async prompting(){
+        this.entity = await this.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Your entity name"
+            }
+        ]);
+
+        this.addField = await this.prompt([
+            {
+                type: 'confirm',
+                name: 'confirmation',
+                message: 'Want to add a field?'
+            }
+        ]);
     }
 
-    method2(){
-        this.log('method 2 just ran');
+    fieldPrompt(){
+        this.log('fields--->');
+        this.log(this.fields);
+        if(this.addField.confirmation){
+            this._addFieldName();
+        }
     }
+
+     async _addFieldName(){
+        const fieldInfo = await this.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Field name"
+            },
+            {
+                type: "list",
+                name: "type",
+                message: "What is the field type?",
+                choices: ['String','Integer','Long','Float','Double','BigDecimal','LocalDate','Instant','ZonedDateTime','Duration','UUID','Boolean','Blob']
+            },
+            {
+                type: "confirmation",
+                name: "required",
+                message: "Is it required?"
+            }
+        ]);
+
+        this.fields.push(fieldInfo);
+
+        this.addField = await this.prompt([
+             {
+                 type: 'confirm',
+                 name: 'confirmation',
+                 message: 'Want to add a field?'
+             }
+         ]);
+        this.fieldPrompt();
+    }
+
 }
